8cc14017f887bf6af94ed68d96dd12254449fac8
Nathan Friedly <nathan@nfriedly.com>
2013-12-09 11:23:33 -0500

    initial commit

---
+++ odb-ii.js
@@ -0,0 +1,46 @@
+Codes = new Meteor.Collection("codes");
+
+if (Meteor.isClient) {
+  Template.results.codes = function () {
+    var query = Session.get("query")
+    // todo: figure out if there's a better way to do this
+    var search = query ? {$where: function() {
+        return this.code.indexOf(query) != -1;
+    }} : {}
+    return Codes.find(search);
+  };
+  
+  function updateQuery() {
+    var query =  document.getElementById("query").value;
+    Session.set("query", query);
+  }
+  
+  Template.search.events({
+    //'keyup input': updateQuery, todo: make this not lag the system, then re-enable it
+    'click button': updateQuery
+  });
+  
+  Template.search.query = function() {
+    return Session.get("query") || "";
+  }
+  
+  Template.results.isMatch = function() {
+    return Template.results.codes().count() != 0;
+  }
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    if (Codes.find({}).count() === 0) {
+        var data = JSON.parse(Assets.getText("odb-ii.json"));
+        
+        // todo: see if there's a better way to do this
+        for (var i = 0; i < data.length; i++) {
+            Codes.insert(data[i]);
+        }
+        console.log("seeded database with " + i + " rows");
+    }
+
+  });
+}
