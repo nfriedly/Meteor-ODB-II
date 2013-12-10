26c883064c153c2c9cb826b8c08c55795e6acfd4
Nathan Friedly <nathan@nfriedly.com>
2013-12-10 10:36:40 -0500

    trying to see if travis ci has expect installed anywhere

---
+++ deploy/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env expect
+
+spawn meteor deploy odb-ii
+expect "Password: "
+# hide expect's output so it's not sharing my password with the world
+log_user 0
+send "$env(PASS)\n"
+log_user 1
+interact
