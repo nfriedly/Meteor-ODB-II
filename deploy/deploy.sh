4e4b42d74c8a39696e0e4e72ff7a55ee638272f0
Nathan Friedly <nathan@nfriedly.com>
2014-03-04 11:35:26 -0500

    Updating deploy script to use meteor account instead of old deploy system

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env expect
 
 spawn meteor deploy odb-ii
+expect "Email: "
+send "$env(EMAIL)\n"
 expect "Password: "
 # hide expect's output so it's not sharing my password with the world
 log_user 0
